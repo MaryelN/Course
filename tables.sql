e416ef9dfd83a100c59b752f0b286b11cc4bbb8d
MaryelN <maryel.navarromtz@gmail.com>
2024-01-27 21:43:59 +0100

    connexion with the database and creation of tables

---
+++ tables.sql
@@ -0,0 +1,43 @@
+
+CREATE TABLE circuit(
+    circuit_id INTEGER AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    pays VARCHAR(50),
+    nom VARCHAR(50),
+    longueur FLOAT
+);
+
+CREATE TABLE course(
+    course_id INTEGER AUTO_INCREMENT,
+    CONSTRAINT course_PK PRIMARY KEY (course_id),
+    nom VARCHAR(255),
+    date_course DATE,
+    circuit_id INTEGER,
+    CONSTRAINT course_FK FOREIGN KEY (circuit_id) REFERENCES circuit(circuit_id)
+);
+
+CREATE TABLE equipe(
+    equipe_id INTEGER AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    nom VARCHAR(50),
+    pays VARCHAR(50),
+    directeur_technique VARCHAR(100)
+);
+
+CREATE TABLE pilote(
+    pilote_id INTEGER AUTO_INCREMENT UNIQUE,
+    CONSTRAINT pilote_PK PRIMARY KEY (pilote_id),
+    nom VARCHAR(50),
+    prenom VARCHAR(50),
+    date_naissance DATE,
+    nationalite VARCHAR(50),
+    equipe_id INTEGER,
+    FOREIGN KEY (equipe_id) REFERENCES equipe(equipe_id)
+);
+
+CREATE TABLE course_pilote(
+    pilote_id INTEGER,
+    course_id INTEGER,
+    position_pilote INTEGER NOT NULL,
+    CONSTRAINT course_pilote_FK FOREIGN KEY (pilote_id) REFERENCES pilote(pilote_id),
+    FOREIGN KEY (course_id) REFERENCES course(course_id),
+    CONSTRAINT course_pilote_PK PRIMARY KEY (pilote_id, course_id)
+);
